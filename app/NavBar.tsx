874c6ae085f7abffe070a51e65b3c6ff80898e52
danie <danielagurcia90@gmail.com>
2025-10-14 17:59:42 -0600

    Style active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,13 @@
+"use client"
 import Link from "next/link"
 import React from "react"
 import { GiAlienBug } from "react-icons/gi"
+import classnames from "classnames"
+import { usePathname } from "next/navigation"
 
 const NavBar = () => {
-  const Links = [
+  const currentPath = usePathname()
+  const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ]
@@ -14,10 +18,15 @@ const NavBar = () => {
         <GiAlienBug className="h-9 w-9 text-teal-600" />
       </Link>
       <ul className="flex space-x-6">
-        {Links.map((link, index) => (
+        {links.map((link, index) => (
           <li
             key={index}
-            className="text-zinc-500 font-medium hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-500": link.href !== currentPath,
+              "text-zinc-800 border-b-2 border-teal-600":
+                link.href === currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             <Link href={link.href}>{link.label}</Link>
           </li>
