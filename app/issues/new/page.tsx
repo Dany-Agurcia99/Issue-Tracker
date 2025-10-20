5e1d8e9ef9ed70a7b0c824f99e00f733255ea9ca
danie <danielagurcia90@gmail.com>
2025-10-20 13:52:21 -0600

    Enhance issue creation validation messages and improve error handling in the new issue form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 "use client"
-import { Button, TextField } from "@radix-ui/themes"
+import { Button, Callout, TextField } from "@radix-ui/themes"
 import SimpleMDE from "react-simplemde-editor"
 import "easymde/dist/easymde.min.css"
 import { useForm, Controller } from "react-hook-form"
 import axios from "axios"
 import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 interface IssueFormData {
   title: string
@@ -14,28 +15,42 @@ interface IssueFormData {
 const NewIssuePage = () => {
   const { register, handleSubmit, control } = useForm<IssueFormData>()
   const router = useRouter()
+  const [error, setError] = useState<string | null>(null)
+
   const onSubmit = async (data: IssueFormData) => {
-    await axios.post("/api/issues", data)
-    router.push("/issues")
+    try {
+      await axios.post("/api/issues", data)
+      router.push("/issues")
+    } catch (error) {
+      setError("Failed to create issue.")
+    }
   }
 
   return (
-    <form className="justify-items-center" onSubmit={handleSubmit(onSubmit)}>
-      <div className="space-y-3 w-1/2 rounded-lg mt-20">
-        <TextField.Root placeholder="Issue Title" {...register("title")} />
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Issue Description" {...field} />
-          )}
-        />
+    <div className="max-w-3xl mx-auto py-10">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
 
-        <div className="flex justify-center">
-          <Button>Submit New Issue</Button>
+      <form className="" onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-3 mt-8">
+          <TextField.Root placeholder="Issue Title" {...register("title")} />
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Issue Description" {...field} />
+            )}
+          />
+
+          <div className="flex justify-center">
+            <Button>Submit New Issue</Button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   )
 }
 
