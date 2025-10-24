79ad7e9e829cd5634577d8cdba96077feec9be7a
danie <danielagurcia90@gmail.com>
2025-10-24 11:32:32 -0600

    Refactor IssueDetail component to enhance layout and add edit functionality;

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { IssueStatusBadge } from "@/app/components"
 import { prisma } from "@/prisma/client"
-import { Card, Flex, Heading, Text } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 import ReactMarkdown from "react-markdown"
+import { Pencil2Icon } from "@radix-ui/react-icons"
 
 interface Props {
   params: {
@@ -19,16 +21,24 @@ const IssueDetail = async ({ params }: Props) => {
   if (!issue) notFound()
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="2" align="center" mb="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toLocaleString()}</Text>
-      </Flex>
-      <Card className="prose" mt="6">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="2" align="center" mb="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toLocaleString()}</Text>
+        </Flex>
+        <Card className="prose" mt="6">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   )
 }
 
