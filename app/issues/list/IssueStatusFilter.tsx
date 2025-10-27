0e691af69a17db16c5c566f26015a4ccefec903a
danie <danielagurcia90@gmail.com>
2025-10-27 16:50:18 -0600

    Refactor IssueActions and add IssueStatusFilter component for improved issue filtering

---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { Status } from "@prisma/client"
+import { Select } from "@radix-ui/themes"
+import { useRouter } from "next/navigation"
+
+const STATUSES: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
+]
+
+const IssueStatusFilter = () => {
+  const router = useRouter()
+  return (
+    <Select.Root
+      onValueChange={(status) => {
+        console.log(status)
+        const query = status === "all" ? "" : `?status=${status}`
+        router.push("/issues/list" + query)
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {STATUSES.map((status) => (
+          <Select.Item key={status.label} value={status.value || "all"}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  )
+}
+
+export default IssueStatusFilter
