8272dc3f32d8033ce1bbcc7898d25123b2be14ac
danie <danielagurcia90@gmail.com>
2025-10-27 15:08:11 -0600

    Refactor AssigneeSelect to use React Query for user fetching and loading state management

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,25 +1,32 @@
 "use client"
 import { User } from "@prisma/client/wasm"
 import { Select } from "@radix-ui/themes"
+import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
+import { Skeleton } from "@/app/components"
 import React, { useEffect } from "react"
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = React.useState<User[]>([])
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users")
-      setUsers(data)
-    }
-    fetchUsers()
-  }, [])
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    retry: 3,
+    staleTime: 2 * 60 * 1000, // 2 minutes
+  })
+
+  if (error) return null
+  if (isLoading) return <Skeleton height={40} />
   return (
     <Select.Root>
       <Select.Trigger placeholder="Select an assignee" />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
