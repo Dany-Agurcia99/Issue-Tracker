f691f786d14f64717b4a6369ca3c3a9d48a224ae
danie <danielagurcia90@gmail.com>
2025-10-27 16:08:09 -0600

    Enhance AssigneeSelect component to update assignee and display success/error notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,12 @@
 "use client"
-import { User } from "@prisma/client/wasm"
+import { Issue, User } from "@prisma/client/wasm"
 import { Select } from "@radix-ui/themes"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { Skeleton } from "@/app/components"
-import React, { useEffect } from "react"
+import toast, { Toaster } from "react-hot-toast"
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -20,20 +20,39 @@ const AssigneeSelect = () => {
 
   if (error) return null
   if (isLoading) return <Skeleton height={40} />
+
   return (
-    <Select.Root>
-      <Select.Trigger placeholder="Select an assignee" />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId ?? "unassigned"}
+        onValueChange={async (userId) => {
+          try {
+            await axios.patch("/api/issues/" + issue.id, {
+              assignedToUserId: userId != "unassigned" ? userId : null,
+            })
+            toast.success("Assignee updated successfully")
+          } catch (error) {
+            toast.error("Failed to update assignee")
+          }
+        }}
+      >
+        <Select.Trigger placeholder="Select an assignee" />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item key="unassigned" value="unassigned">
+              Unassigned
             </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   )
 }
 
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -32,7 +32,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex gap="3" direction="column">
-            <AssigneeSelect />
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
