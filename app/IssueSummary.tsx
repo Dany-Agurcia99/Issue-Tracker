ee1a398c3d07ac5c23b26f9627182f382de93ed5
danie <danielagurcia90@gmail.com>
2025-10-28 14:52:43 -0600

    Implement IssueSummary component to display counts of open, in-progress, and closed issues

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,35 @@
+import { Status } from "@prisma/client"
+import { Card, Flex, Text } from "@radix-ui/themes"
+import Link from "next/link"
+interface IssueSummaryProps {
+  open: number
+  inProgress: number
+  closed: number
+}
+const IssueSummary = ({ open, inProgress, closed }: IssueSummaryProps) => {
+  const statuses: { label: string; count: number; status: Status }[] = [
+    { label: "Open Issues", count: open, status: "OPEN" },
+    {
+      label: "Issues In Progress ",
+      count: inProgress,
+      status: "IN_PROGRESS",
+    },
+    { label: "Closed Issues", count: closed, status: "CLOSED" },
+  ]
+  return (
+    <Flex gap="4">
+      {statuses.map((status) => (
+        <Card key={status.label}>
+          <Flex direction="column" gap="1" align="center" p="4">
+            <Link href={`/issues/list?status=${status.status}`}>{status.label}</Link>
+            <Text size="6" className="font-bold">
+              {status.count}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  )
+}
+
+export default IssueSummary
